f94505375feb1e2cfb23af2ac2fde77e75abb94c
binyamin.regev <beniregev@gmail.com>
2021-12-03 20:18:45 +0200

    Initial Version - Adding project Application class

---
+++ src/main/java/com/beniregev/LanguageDictationApp/LanguageDictationAppApplication.java
@@ -0,0 +1,13 @@
+package com.beniregev.LanguageDictationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageDictationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageDictationAppApplication.class, args);
+    }
+
+}
